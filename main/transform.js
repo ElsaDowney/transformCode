e7c3c59ee2d495bc2f364489be7f39b6a8a15705
ElsaDowney <446189226@qq.com>
2016-07-23 09:17:41 +0800

    turnToCODE

--- main/transform.js
+++ main/transform.js
@@ -1,10 +1,105 @@
-function turnToZipcode(inputs) {
+function turnToBarcode(Zipcode, codeArray) {
 
-    const isText = checkText(inputs);
+    const isZipcode = checkZipcode(Zipcode);
+    if (isZipcode) {
+        const Code = buildTransform(Zipcode, codeArray);
+        const barcode = buildBarcode(Code);
+        return barcode;
+    }
+    else
+        return '输入错误，请检查！';
 }
 
-function checkText(inputs) {
-    const text =inputs.replace('-','');
-    const length = text.length;
-    console.log(text);
+function checkZipcode(Zipcode) {
+
+    let flag = false;
+    if (Zipcode.length == 32 || Zipcode.length == 52) {
+        Zipcode.split('').find(zipcode=> {
+            if (zipcode == '|' || zipcode == ':') {
+                flag = true;
+            }
+        });
+    }
+    return flag;
 }
+
+function buildBarcode(code) {
+
+    var result = '';
+    if (code.length == 5) {
+        result = code.join('');
+    }
+    else {
+        result = code.join('');
+        result = result.substr(0, 5) + '-' + result.substr(5, 9);
+    }
+    return result;
+}
+
+function buildTransform(zipcode, codeArray) {
+
+    var Code = [];
+    let index = 1;
+    while (index + 5 < zipcode.length - 1) {
+        let codeNum = zipcode.substr(index, 5);
+        index += 5;
+        for (let j = 0; j < codeArray.length; j++) {
+            if (codeNum === codeArray[j]) {
+                Code.push(j.toString());
+            }
+        }
+    }
+    return Code;
+}
+
+function turnToZipcode(Barcode, codeArray) {
+
+    const isBacode = checkBarcode(Barcode);
+    if (isBacode) {
+        const code = buildColumn_Weight(Barcode);
+        const zipcode = buildZipcode(code, codeArray);
+        console.log(zipcode);
+    }
+    else
+        return '输入错误，请检查！';
+
+}
+
+function buildZipcode(code, codeArray) {
+
+    const zipcode = code.split('').map(i => codeArray[i]).join('');
+
+    return `|${zipcode}|`;
+}
+
+function buildColumn_Weight(Barcode) {
+
+    const column_weight = (Barcode.split('').map(str => parseInt(str)).reduce((a, b) => a + b)) % 10;
+    const codeText = Barcode + column_weight;
+
+    return codeText;
+}
+
+function checkBarcode(Barcode) {
+    var flag = false;
+    const text = Barcode.replace('-', '');
+    if (text.length == 5 || text.length == 9) {
+        text.split('').find(num => {
+            if (parseInt(num) >= 0 && parseInt(num) <= 9) {
+                flag = true;
+            }
+        });
+    }
+    return flag;
+}
+
+module.exports = {
+    turnToZipcode: turnToZipcode,
+    checkBarcode: checkBarcode,
+    buildColumn_Weight: buildColumn_Weight,
+    buildZipcode: buildZipcode,
+    turnToBarcode: turnToBarcode,
+    checkZipcode: checkZipcode,
+    buildTransform: buildTransform,
+    buildBarcode: buildBarcode
+};
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,22 +1,85 @@
 'use strict';
 var main = require('../main/transform.js');
 
-describe('transformCode',function () {
+describe('transformBarode', function () {
 
-    const inputs = '95713';
+    const Zipcode = '|:|::|:|:|:||::::|:|::||:::::||::|:|::||::|::|||:::|';
 
-    it('turnToZipcode',function () {
+    const CodeArray = ['||:::', ':::||', '::|:|', '::||:', ':|::|', ':|:|:', ':||::', '|:::|', '|::|:', '|:|::'];
 
-        const expectText = ' ||:|:::|:|:|:::|:::||::||::|:|:|';
+    it('turnToBarcode', function () {
 
-        const zipCode = turnToZipcode(inputs);
+        const ExpectText = '45056-1234';
+        const aaa = main.turnToBarcode(Zipcode, CodeArray);
 
-        expect(zipCode).toEqual(expectText);
+        expect(aaa).toEqual(ExpectText);
     });
 
-    it('checkInput',function () {
-        
-        const checkText = checkInput(inputs);
-        
+    it('checkZipcode', function () {
+
+        const checkFT = main.checkZipcode(Zipcode);
+        const flag = true;
+
+        expect(checkFT).toBe(flag);
     });
+
+    it('buildTransform', function () {
+
+        const codeNum = main.buildTransform(Zipcode, CodeArray);
+        const expectCode = ['4', '5', '0', '5', '6', '1', '2', '3', '4'];
+
+        expect(codeNum).toEqual(expectCode);
+    });
+
+    it('buildBarcode', function () {
+
+        const barcode = main.buildBarcode(main.buildTransform(Zipcode, CodeArray));
+        const result = '45056-1234';
+
+        expect(barcode).toEqual(result);
+    });
+});
+
+
+describe('transformZipcode', function () {
+
+    const Barcode = '95713';
+
+    const CodeArray = ['||:::', ':::||', '::|:|', '::||:', ':|::|', ':|:|:', ':||::', '|:::|', '|::|:', '|:|::', '||:::'];
+
+    it('turnToZipcode', function () {
+
+        const expectText = '||:|:::|:|:|:::|:::||::||::|:|:|';
+
+        spyOn(console, 'log');
+
+        main.turnToZipcode(Barcode, CodeArray);
+
+        expect(console.log).toHaveBeenCalledWith(expectText);
+    });
+
+    it('checkInput', function () {
+
+        const checkTF = main.checkBarcode(Barcode);
+
+
+        expect(checkTF).toBeTruthy();
+    });
+
+    it('buildColumn_Weight', function () {
+
+        const code = '957135';
+        const column_weight = main.buildColumn_Weight(Barcode);
+
+        expect(column_weight).toEqual(code);
+    });
+
+    it('buildZipcode', function () {
+
+        const zipCode = main.buildZipcode(main.buildColumn_Weight(Barcode), CodeArray);
+        const codeZip = '||:|:::|:|:|:::|:::||::||::|:|:|';
+
+        expect(zipCode).toEqual(codeZip);
+    });
+
 });
